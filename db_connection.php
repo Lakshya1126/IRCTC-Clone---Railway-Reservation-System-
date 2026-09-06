f98d49916db0668a0f1e5d88e8666acfa5c6c16f
Lakshya Kansal <lakshyakansal92@gmail.com>
2026-09-06 23:16:01 +0530

    Create db_connection.php

---
+++ db_connection.php
@@ -0,0 +1,14 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$dbname = "irctc_clone";
+
+
+$conn = new mysqli($host, $user, $pass, $dbname);
+
+
+if ($conn->connect_error) {
+die("Connection failed: " . $conn->connect_error);
+}
+?>
